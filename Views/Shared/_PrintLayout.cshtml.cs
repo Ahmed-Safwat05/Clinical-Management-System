908a779e47c9195d1db50ba18c3d5356d4e61cdc
Ahmed-Safwat05 <safwata677@gmail.com>
2026-06-22 19:09:50 +0300

    feat(printing): add professional printing system with dynamic clinic branding

---
+++ Views/Shared/_PrintLayout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ClinicManagementSystem.Views.Shared
+{
+    public class _PrintLayoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
